6920e70b789fa04c63dff8f2236abf7b7c9f91ba
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-05-14 23:37:01 +0630

    shadcn setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const Header = () => {
+  return (
+    <div> <Button variant='destructive' >Button</Button></div>
+  )
+}
+
+export default Header
