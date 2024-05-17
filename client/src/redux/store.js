a698d084de3fa82cc61ce564c138edf15886d33b
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-05-17 10:34:29 +0630

    save user to localstorage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import persistReducer from 'redux-persist/es/persistReducer'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore'
 
-export const store = configureStore({
-    reducer: {
-        user:userReducer
-    },
+const rootReducer = combineReducers({
+    user: userReducer
 })
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+// combine all the reducers and pass in the persistConfig 
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = configureStore({
+    reducer: persistedReducer,
+    // prevent error using redux 
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+})
+
+export const persistor = persistStore(store)
